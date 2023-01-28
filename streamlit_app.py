2472b58b26d7a232098214695baf6616c071618a
marinarsh <123435330+marinarsh@users.noreply.github.com>
2023-01-28 17:50:41 -0600

    Update streamlit_app.py
    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title ('Hello World')
-
-streamlit.text ('Omega 3 food')
-
-streamlit.text('meow again')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
