b8d45f6e78acd13bea91705f3626db3bdd6ad853
marinarsh <123435330+marinarsh@users.noreply.github.com>
2023-02-01 20:16:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" =  "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  "kiwi")
 # streamlit.text(fruityvice_response.json()) #This  .json actually showed json version of the response as opposed to   showing <response 200>
 
 # write your own comment passed the response to pandas library
