f480112515b7b00eb37a281e1bdca6eed173c7c0
marinarsh <123435330+marinarsh@users.noreply.github.com>
2023-02-01 20:16:28 -0600

    Update streamlit_app.py
    + kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #This  .json actually showed json version of the response as opposed to   showing <response 200>
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" =  "kiwi")
+# streamlit.text(fruityvice_response.json()) #This  .json actually showed json version of the response as opposed to   showing <response 200>
 
-# write your own comment -what does the next line do? 
+# write your own comment passed the response to pandas library
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# write your own comment - this shows the response in a  table 
 streamlit.dataframe(fruityvice_normalized)
